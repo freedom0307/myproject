b66d9889bc83559c77bcfb5dceba34c5d4d63bc7
freedom <1162113236@qq.com>
2016-09-22 22:20:49 +0800

    修改

---
+++ serialization/IO/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serialization.IO
+{
+    [Serializable ]
+    public  class Person
+    {
+        public int  ID=1;
+        public string name { get; set; }
+        public string sex { get; set; }
+
+    }
+    [Serializable]
+    public class Programmer:Person
+    {
+        public string language { get; set; }
+    }
+}
---
+++ serialization/IO/SearealizeHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace serialization.IO
+{
+    public  class SearealizeHelper
+    {
+        public static List<Programmer > GetProgrammerList()
+        {
+            List<Programmer> Plist = new List<IO.Programmer>();
+            Plist.Add(new IO.Programmer() { name = "快乐阳光",sex = "男"} );
+            Plist.Add(new IO.Programmer() { name = "荼蘼", sex = "女" });
+            Plist.Add(new IO.Programmer() { name = "笑对人生", sex = "男" });
+            Plist.Add(new IO.Programmer() { name = "萌萌的", sex = "男" });
+            Plist.Add(new IO.Programmer() { name = "红领巾", sex = "男" });
+            return Plist;
+        }
+        public static string CurrentDataPath = ConfigurationManager.AppSettings["CurrentDataPath"];
+
+        public static void BinarySerialize()
+        {
+            string filename = Path.Combine(CurrentDataPath, @"BinarySerialization.txt");
+            using (FileStream fs = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                List<Programmer> Plsit = GetProgrammerList();
+                bf.Serialize(fs, Plsit );
+            }
+            using (FileStream fs = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                List<Programmer> Plsit = GetProgrammerList();
+                Plsit.Clear();
+                fs.Position = 0;
+                Plsit =(List <Programmer >) bf.Deserialize (fs );
+            }
+        }
+
+    }
+}
